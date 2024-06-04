09a6fb4e0bd98f7ddf7bf77c6b3a5b68ceb59a39
mj99k14 <163389869+mj99k14@users.noreply.github.com.>
2024-06-04 20:55:51 +0900

    연습장

---
+++ 연습/6.4.py
@@ -0,0 +1,7 @@
+avga = 0
+scores = [92, 85, 34, 76, 58, 90, 61, 70, 45, 99, 82, 67, 50, 77, 89]
+# a = [i for i in scores if i >= 90 ]
+# for a1 in a:
+#     avga += a1 / len(a) 
+# print(f"A 등급: 평균 점수 ={format(avga,".2f")}")
+
