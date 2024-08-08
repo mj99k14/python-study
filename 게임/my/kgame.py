65dd2c8a4171c43b7bcf113a3ad22a87ab72227d
mj99k14 <ekfkawnl644@gmail.com>
2024-08-08 12:12:28 +0900

    8.8

--- 게임/my/kgame.py
+++ 게임/my/kgame.py
@@ -2,7 +2,7 @@ import pygame
 
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("벽돌꺠기")
+pygame.display.set_caption("성식이 구하기 ")
 #색상 정의
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -11,15 +11,20 @@ BLUE = (0, 0, 255)
 
 #게임 패들 
 # 패들 설정
-paddle_width = 200
+paddle_width = 100
 paddle_height = 10
 paddle_speed = 500
 paddle = pygame.Rect(350, 550, paddle_width, paddle_height)
-# ball_image = pygame.image.load("red_rect.png")
+
+
+The_end = pygame.image.load("theend.png")
+The_end_rect = The_end.get_rect()
+The_end_rect.topleft = (400,300)
 # 공 설정
-ball_radius = 10
+ball_image = pygame.image.load("kkk.png")
+ball_rect = ball_image.get_rect()
+ball_rect.topleft =(350, 350)
 ball_speed = [5, -5]
-ball = pygame.Rect(700, 300, ball_radius * 2, ball_radius * 2)
 # 게임 속도 설정
 clock = pygame.time.Clock()
 fps = 60
@@ -58,37 +63,47 @@ while running:
         paddle.y = 0
         paddle_speed = -paddle_speed       
     # 공 이동 
-    ball.x += ball_speed[0]
-    ball.y += ball_speed[1]
+    ball_rect.x += ball_speed[0]
+    ball_rect.y += ball_speed[1]
+
     #공이 화면밖에 나가지않도록
-    if ball.x + ball.width > screen.get_width():
-        ball.x = screen.get_width() - ball.width
-        ball_speed = -ball_speed 
-    if ball.x < 0:
-        ball.x = 0
-        ball_speed = -ball_speed
-    #위아래 부딪힘
-    # 공과 벽 충돌
-    if ball.left <= 0 or ball.right >= screen.get_width():
+    if ball_rect.x + ball_rect.width > screen.get_width():
+        ball_rect.x = screen.get_width() - ball_rect.width
         ball_speed[0] = -ball_speed[0]
-    if ball.top <= 0:
+    if ball_rect.x < 0:
+        ball_rect.x = 0
+        ball_speed[0] = -ball_speed[0]
+    #위아래 부딪힘
+    
+    # 공과 벽 충돌
+    if ball_rect.left <= 0 or ball_rect.right >= screen.get_width():
+        ball_speed[0] = -ball_speed[0]
+    if ball_rect.top <= 0:
         ball_speed[1] = -ball_speed[1]
         
 
     #공이랑 패드가 부딪힘
-    if ball.colliderect(paddle):
+    if ball_rect.colliderect(paddle):
         ball_speed[1] = -ball_speed[1]
         ball_count += 1
-        print(f"횟수는:{ball_count} 입네다~")
+        print(f"COUNT:{ball_count} ")
+        ball_speed[0] *1.1
+        ball_speed[1]* 1.1
+        
         
     
     #공이 바닥이랑 부딪힘
-    if ball.bottom >= screen.get_height():
+    if ball_rect.bottom >= screen.get_height():
+        screen.fill(BLACK)
+        screen.blit(The_end,The_end_rect) #_> 이미지를 화면에 그리는 데 사용 screen.blit(image, (x, y))
+        pygame.display.flip()
+        pygame.time.wait(3000)
         running = False
-
-    screen.fill((255, 255, 255))
+        
+   
     screen.fill(BLACK)
-    pygame.draw.ellipse(screen, WHITE, ball)
+    screen.blit(ball_image,ball_rect)
+    # pygame.draw.ellipse(screen, WHITE, ball_rect)
     pygame.draw.rect(screen, (0, 0, 255), paddle) # Rect 객체 이용
     pygame.display.flip()
     
